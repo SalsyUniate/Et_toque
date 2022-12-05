2f92013c61a6b938208ae2a226896218facd585f
Cloé Léglise <cloe.leglise@free.fr>
2022-12-05 10:56:59 +0100

    modif erreur super constructeur

--- src/Moderateur.java
+++ src/Moderateur.java
@@ -3,11 +3,7 @@ import java.util.ArrayList;
 public class Moderateur extends Inscrit{
 	
 	public Moderateur(String pseudo,String mdp, String contact, ArrayList<Posts> post,boolean statusConnexion) {
-		this.pseudo = pseudo; 
-		this.mdp = mdp;
-		this.contact = contact;
-		this.post = post;
-		this.statusConnexion = statusConnexion;
+		super();
 	}
 	void validerPost(Posts post){
 		post.Valider();
